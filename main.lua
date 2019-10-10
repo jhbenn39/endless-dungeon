0257f3b9fd4bcfd9ab277d65d5c08a19820e0efd
JohnHBenn <jhbenn39@harrisonburg.k12.va.us>
2019-10-10 09:55:28 -0400

    dsadsa

--- main.lua
+++ main.lua
@@ -2,44 +2,56 @@ love.graphics.setDefaultFilter('nearest', 'nearest')
 local Map = require 'core/map'
 
 function love.load()
-  map = Map:new(12, 12)
+  map = Map:new(0, 0)
   x = 400
   y = 300
-  playerImg = love.graphics.newImage('assets-1/player/base/octopode_1.png')
-  floor = love.graphics.newImage('assets-1/dark_floor.png')
-flour = {
-          {floor, floor, floor, floor, floor, floor, floor, floor, floor, floor, floor, floor},
-          {floor, floor, floor, floor, floor, floor, floor, floor, floor, floor, floor, floor},
-          {floor, floor, floor, floor, floor, floor, floor, floor, floor, floor, floor, floor},
-          {floor, floor, floor, floor, floor, floor, floor, floor, floor, floor, floor, floor},
-          {floor, floor, floor, floor, floor, floor, floor, floor, floor, floor, floor, floor},
-          {floor, floor, floor, floor, floor, floor, floor, floor, floor, floor, floor, floor},
-          {floor, floor, floor, floor, floor, floor, floor, floor, floor, floor, floor, floor},
-          {floor, floor, floor, floor, floor, floor, floor, floor, floor, floor, floor, floor},
-          {floor, floor, floor, floor, floor, floor, floor, floor, floor, floor, floor, floor},
-          {floor, floor, floor, floor, floor, floor, floor, floor, floor, floor, floor, floor},
-          {floor, floor, floor, floor, floor, floor, floor, floor, floor, floor, floor, floor},
-          {floor, floor, floor, floor, floor, floor, floor, floor, floor, floor, floor, floor}
+  playerImg = love.graphics.newImage('mdbyme/player.png')
+  wv = love.graphics.newImage('mdbyme/white_void.png')
+  bed = love.graphics.newImage('mdbyme/bed.png')
+  mv = love.graphics.newImage('mdbyme/mid_void.png')
+  dv = love.graphics.newImage('mdbyme/dark_floor.png')
+floor = {
+          {wv, wv, wv, wv, wv, wv, wv, wv, wv, wv, wv, wv},
+          {wv, wv, wv, wv, wv, wv, wv, wv, wv, wv, wv, wv},
+          {wv, wv, wv, wv, wv, wv, wv, wv, wv, wv, wv, wv},
+          {wv, wv, wv, wv, wv, wv, wv, wv, wv, wv, wv, wv},
+          {wv, wv, wv, wv, wv, wv, wv, wv, wv, wv, wv, wv},
+          {wv, wv, wv, wv, wv, wv, wv, wv, wv, wv, wv, wv},
+          {wv, wv, wv, wv, wv, wv, wv, wv, wv, wv, wv, wv},
+          {wv, wv, wv, wv, wv, wv, wv, wv, wv, wv, wv, wv},
+          {wv, wv, wv, wv, wv, wv, wv, wv, wv, wv, wv, wv},
+          {wv, wv, wv, wv, wv, wv, wv, wv, wv, wv, wv, wv},
+          {wv, wv, wv, wv, wv, wv, wv, wv, wv, wv, wv, wv},
+          {wv, wv, wv, wv, wv, wv, wv, wv, wv, wv, wv, wv}
         }
-map = Map:new(flour)
+map = Map:new(floor)
 end
 function love.update(dt)
- if love.keyboard.isDown('w')then
+ if love.keyboard.isDown('W')then
     y = y - 1
   end
-  if love.keyboard.isDown('a')then
+  if love.keyboard.isDown('A')then
     x = x - 1
   end
-  if love.keyboard.isDown('s')then
+  if love.keyboard.isDown('S')then
     y = y + 1
   end
-  if love.keyboard.isDown('d')then
+  if love.keyboard.isDown('D')then
     x = x + 1
   end
 end
 
 function love.update(dt)
-  if love.keyboard.isDown('right') then
+  if love.keyboard.isDown('up')then
+    y = y - 1
+  end
+  if love.keyboard.isDown('left')then
+    x = x - 1
+  end
+  if love.keyboard.isDown('down')then
+    y = y + 1
+  end
+  if love.keyboard.isDown('right')then
     x = x + 1
   end
 end
