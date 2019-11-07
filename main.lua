920b630d0e5186f6b09f12987eabf350ae9de717
JohnHBenn <jhbenn39@harrisonburg.k12.va.us>
2019-11-07 09:54:32 -0500

    yay?
    yay?

--- main.lua
+++ main.lua
@@ -15,12 +15,14 @@ function love.load()
   walk2 = anim8.newAnimation(grid('1-3', 2), 0.2)
   walk3 = anim8.newAnimation(grid('1-3', 3), 0.2)
   walk4 = anim8.newAnimation(grid('1-3', 4), 0.2)
-  idle = love.graphics.newAnimation(grid('1', 1), 0.2)
+  idle = anim8.newAnimation(grid('1-3', 5), 0.2)
   wv = love.graphics.newImage('mdbyme/white_void.png')
   bed = love.graphics.newImage('mdbyme/bed.png')
   mv = love.graphics.newImage('mdbyme/mid_void.png')
   dv = love.graphics.newImage('mdbyme/dark_floor.png')
-  
+  spirit = love.graphics.newImage('mdbyme/spirit.png')
+  fountain = love.graphics.newImage('mdbyme/fountain.png')
+  goblet = love.graphics.newImage('mdbyme/goblet.png')
 
 floor = {
           {wv, wv, wv, wv, wv, wv, wv, wv, wv, wv, wv, wv, wv, wv, wv, wv, wv, wv, wv, wv, wv, wv, wv, wv},
@@ -59,9 +61,9 @@ collision = {
   {clw, clw, clw, clw, clw, clw, clw, clw, clw, clw, clw, clw, clw, clw, clw, clw, clw, clw, clw, clw, clw, clw, clw, clw},
   {clw, 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', clw},
   {clw, 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', clw},
+  {clw, 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', fountain, 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', clw},
   {clw, 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', clw},
-  {clw, 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', clw},
-  {clw, 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', bed, 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', clw},
+  {clw, 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', spirit, 'nil', 'nil', 'nil', bed, 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', clw},
   {clw, 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', clw},
   {clw, 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', clw},
   {clw, 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', clw},
@@ -79,7 +81,7 @@ collision = {
   {cld, 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', cld},
   {cld, 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', cld},
   {cld, 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', cld},
-  {cld, 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', cld},
+  {cld, 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', goblet, 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', cld},
   {cld, 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', cld},
   {cld, cld, cld, cld, cld, cld, cld, cld, cld, cld, cld, cld, cld, cld, cld, cld, cld, cld, cld, cld, cld, cld, cld, cld},
 }
@@ -90,7 +92,10 @@ end
 
 
 function love.update(dt)
-  if love.keyboard.isDown('up') or love.keyboard.isDown('w') then
+  if collision:cc(x, y, 64, 64) == true && love.keyboard.isDown('space') then
+    love.graphics.print('Player: Who are you? Where am i? How did I get here?',  100, 100)
+  end
+    if love.keyboard.isDown('up') or love.keyboard.isDown('w') then
     if collision:cc(x, y - 4, 64, 64) == false then
       y = y - 4
       walk2:update(dt)
@@ -126,6 +131,7 @@ function love.draw()
   cam:draw(function(l, t, w, h)
   floor:draw()
   collision:draw()
+  idle:draw(spritesheet, x, y)
   if love.keyboard.isDown('up') or love.keyboard.isDown('w') then
     walk2:draw(spritesheet, x, y)
   end
@@ -141,6 +147,6 @@ function love.draw()
   if love.keyboard.isDown('right') or love.keyboard.isDown('d') then
     walk3:draw(spritesheet, x, y)
   end
-idle:draw(spritesheet, x, y)
+
   end)
 end
